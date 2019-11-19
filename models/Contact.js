53f1ef464a88aef35a90bb19e1673e344ed7613a
Vuthy <suonx002@gmail.com>
2019-11-19 17:04:50 -0600

    set up server for contact

---
+++ models/Contact.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ContactSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Contact = mongoose.model('contact', ContactSchema);
